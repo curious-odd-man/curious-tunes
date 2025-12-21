bd4a58f7d370f769e8d2e84676535fea8b4f5015
curious_odd_man <vlad.varslavans@gmail.com>
2025-12-22 00:21:58 +0200

    display of album tracks implementation
    (cherry picked from commit c67fcbef773590538b0c5687c1493a0c352ba409)

--- curiousTunes/src/main/java/com/github/curiousoddman/curious_tunes/controller/LibraryArtistAlbumController.java
+++ curiousTunes/src/main/java/com/github/curiousoddman/curious_tunes/controller/LibraryArtistAlbumController.java
@@ -1,7 +1,8 @@
 package com.github.curiousoddman.curious_tunes.controller;
 
-import com.github.curiousoddman.curious_tunes.model.Album;
-import com.github.curiousoddman.curious_tunes.model.Track;
+import com.github.curiousoddman.curious_tunes.backend.DataAccess;
+import com.github.curiousoddman.curious_tunes.dbobj.tables.records.AlbumRecord;
+import com.github.curiousoddman.curious_tunes.dbobj.tables.records.TrackRecord;
 import com.github.curiousoddman.curious_tunes.model.bundle.ArtistAlbumBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,12 +10,15 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -23,23 +27,45 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROT
 @Lazy
 @Component
 @Scope(SCOPE_PROTOTYPE)
+@RequiredArgsConstructor
 public class LibraryArtistAlbumController implements Initializable {
+    private final DataAccess dataAccess;
     public ImageView albumImage;
     public Label albumTitle;
     public Label albumDetails;
     public GridPane albumTracksGrid;
 
     @Override
+    @SneakyThrows
     public void initialize(URL location, ResourceBundle resources) {
         if (resources instanceof ArtistAlbumBundle albumBundle) {
-            Album album = albumBundle.getAlbum();
-            albumTitle.setText(album.getName());
-            albumImage.setImage(new Image(Path.of(album.getImage()).toUri().toString()));
-            albumDetails.setText(album.getTracks().size() + " tracks");
-            List<Track> tracks = album.getTracks();
-            for (int i = 0; i < tracks.size(); i++) {
-                Track track = tracks.get(i);
-                albumTracksGrid.addRow(i, new Label(track.getDuration() + " --> " + track.getName()));
+            AlbumRecord albumRecord = albumBundle.getAlbumRecord();
+            if (albumRecord.getImage() != null) {
+                try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(albumRecord.getImage())) {
+                    albumImage.setImage(new Image(byteArrayInputStream));
+                }
+            }
+
+            albumTitle.setText(albumRecord.getName());
+            albumDetails.setText("empty details..."); // FIXME
+            List<TrackRecord> albumsTracks = dataAccess.getAlbumsTracks(List.of(albumRecord.getId()));
+            int tracksPerColumn = albumsTracks.size() <= 10
+                    ? albumsTracks.size()
+                    : (albumsTracks.size() / 2);
+
+            Iterator<TrackRecord> iterator = albumsTracks.iterator();
+            int row = 0;
+            int col = 0;
+            while (iterator.hasNext()) {
+                TrackRecord trackRecord = iterator.next();
+                Label child = new Label(trackRecord.getTrackNumber() + ": " + trackRecord.getTitle() + " :: " + trackRecord.getDuration());
+                albumTracksGrid.add(child, col, row);
+                if (row + 1 == tracksPerColumn) {
+                    col++;
+                    row = 0;
+                } else {
+                    row++;
+                }
             }
         }
     }
