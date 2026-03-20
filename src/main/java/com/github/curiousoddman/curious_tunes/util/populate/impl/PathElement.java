e66d9e768f48b1ca4eb94f1184f874c6bd837dce
curious_odd_man <vlad.varslavans@gmail.com>
2026-03-20 22:21:29 +0200

    removed unused code

--- src/main/java/com/github/curiousoddman/curious_tunes/util/populate/impl/PathElement.java
+++
@@ -1,7 +0,0 @@
-package com.github.curiousoddman.curious_tunes.util.populate.impl;
-
-public record PathElement(String setterName,
-                          Class<?> type,
-                          Class<?> genericType) {
-
-}
